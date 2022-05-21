3b4ef5d16110c866ef18d34b8558c8ffb8e36d17
unknown <brunomembrado10@gmail.com>
2022-05-21 20:24:04 -0300

    prueba

--- 03-JS-II/homework/tests/JSII.test.js
+++ 03-JS-II/homework/tests/JSII.test.js
@@ -15,7 +15,7 @@ const {
   tablaDelSeis,
   tieneTresDigitos,
   doWhile
-} = require('../homework');
+} = require('../homework(mio)');
 
 
 describe('obtenerMayor(x, y)', function() {
@@ -53,7 +53,7 @@ describe('saludo(idioma)', function() {
   it('should return \'Guten Tag!\' for aleman', function() {
     expect(saludo('aleman')).toBe('Guten Tag!');
   });
-  it('should return \'Hello!\' for ingles', function() {
+  it('should return \'Hola!\' for ingles', function() {
     expect(saludo('ingles')).toBe('Hello!');
   });
   it('should return \'Ni Hao!\' for mandarin', function() {
